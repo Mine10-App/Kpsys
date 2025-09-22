a4a24e814f697a823744abf0c5e5cbdd7cab6f81
Mine <air1345@gmail.com>
2025-09-22 11:58:34 +0500

    Update rate.js

--- rate.js
+++ rate.js
@@ -1,7 +1,7 @@
 const rates = {
   AdultAUSD: { price: 35.00, GST: 8, WP: "Leeli" },
   KidsAUSD: { price: 25.00, GST: 8, WP: "Leeli" },
-  AdultAMVR: { price: 539.10, GST: 8, WP: "Leeli" },
+  AdultAMVR: { price: 539.70, GST: 8, WP: "Leeli" },
   KidsAMVR: { price: 385.50, GST: 8, WP: "Leeli" },
   AdultBUSD: { price: 24.31, GST: 8, WP: "Leeli" },
   KidsBUSD: { price: 24.31, GST: 8, WP: "Leeli" },
