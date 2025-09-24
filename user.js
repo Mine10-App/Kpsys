ce30b504cc32301e46460fd85c4254a4775e53fe
Mine <air1345@gmail.com>
2025-09-24 22:09:52 +0500

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,6 @@
 // user.js
 const users = [
-  { username: "Mohamed", name: "Mohamed Ibrahim", password: "Welcome@1234", rcNo: "623", WP: "Leeli", Level: "Supervisor" },
-  { username: "Hussain", name: "Hussain Ahmed", password: "Welcome@1234", rcNo: "T-0115", WP: "Leeli", Level: "Supervisor" },
-  { username: "Liushadha", name: "Liushadha", password: "Welcome@1234", rcNo: "T", WP: "Leeli", Level: "User" },
-  { username: "Irufan", name: "Abdulla Irufan", password: "Welcome@1234", rcNo: "979", WP: "Leeli", Level: "Supervisor" }
+  { username: "Irufan", name: "Abdulla Irufan", passwordHash: "557808176cfefce664c3694042b720e1be6a41974527178598622edc21dc6d9d", Level: "Supervisor" },
+  { username: "Hussain", name: "Hussain Ahmed", passwordHash: "49cf3a25fc899747dc743651c9d9167ba46f82a5f0ff9ed9d9d90ec8f5fcd740", Level: "User" },
+  { username: "Mohamed", name: "Mohamed Ibrahim", passwordHash: "daa222e89336f6b8866918e196958a1ecac954f0e2f130c67186c9c53f7333c2", Level: "User" }
 ];
