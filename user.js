d2a3ac94e34e411f98d805e460b7b4238db7a41f
Mine <air1345@gmail.com>
2025-09-24 11:08:49 +0500

    Update user.js

--- user.js
+++ user.js
@@ -2,5 +2,6 @@
 const users = [
   { username: "Mohamed", name: "Mohamed Ibrahim", password: "Welcome@1234", rcNo: "623", WP: "Leeli", Level: "Supervisor" },
   { username: "Hussain", name: "Hussain Ahmed", password: "Welcome@1234", rcNo: "T-0115", WP: "Leeli", Level: "Supervisor" },
+  { username: "Liushadha", name: "Liushadha", password: "Welcome@1234", rcNo: "T", WP: "Leeli", Level: "User" },
   { username: "Irufan", name: "Abdulla Irufan", password: "Welcome@1234", rcNo: "979", WP: "Leeli", Level: "Supervisor" }
 ];
