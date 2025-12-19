b065b8e866cb8d10566610d88f8e97575a4d9fde
Mine <air1345@gmail.com>
2025-12-19 14:05:38 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6_tJpSN2ca8L12Rm9ulaosTJfG2SKUK8",
+  authDomain: "kpsyst-a5377.firebaseapp.com",
+  projectId: "kpsyst-a5377",
+  storageBucket: "kpsyst-a5377.firebasestorage.app",
+  messagingSenderId: "738297113997",
+  appId: "1:738297113997:web:1dd012a94ad77c981461c4",
+  measurementId: "G-H60ZMZ6LV6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = firebase.firestore();
+
+console.log("Firebase initialized successfully");
